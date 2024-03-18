0f5a8266069b8f4a83000ce01a11f1ca0e6460d6
samzuza <100250379+samzuza@users.noreply.github.com>
2024-03-17 22:15:55 -0400

    Update adminHomepage.php
    It now accesses the database and needs html for the front end look of things.

--- adminHomepage.php
+++ adminHomepage.php
@@ -1,59 +1,42 @@
+<?php include('include/project_config.php'); ?>
 <html>
 <head>
   <title>Mood Matrix</title>
 </head>
 
 <body>
-<h1>Admin Page</h1>
-<h2>Edit products, add/remove products</h2>
+<h1>Admin Homepage</h1>
+<h2>Edit, add, and remove products</h2>
 
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$db = "product_rating_analysis";
-$sql = mysqli_connect($host, $username, $password, $db);
-if ($sql->connect_error) {
-  die("Connection failed: " . $sql->connect_error);
+$query = mysqli_query($sql, "SELECT * FROM products");
+while($row = mysqli_fetch_assoc($query))
+{
+    $id = $row['id'];
+    $name = $row['name'];
+    $description = $row['description'];
+    
+    echo "<h1>Product Name: $name</h1>";
+    echo "<p> Product ID: $id</p>";
+    echo "<p> Product Description: $description</p>";
+    
+    //Allows admin to change product info, connected to update_product.php
+    echo "<form method='post' action='update_product.php'>";
+    echo "<input type='hidden' name='id' value='$id' />";
+    echo "<input type='text' name='name' value='$name' />";
+    echo "<input type='text' name='description' value='$description' />";
+    echo "<input type='submit' value='Update Product' />";
+    echo "</form>";
+    
+    
+    //Delete item button for admin use, connected to delete_product.php
+    echo "<form method='post' action='delete_product.php'>";
+    echo "<input type='hidden' name='id' value='$id' />";
+    echo "<input type='submit' value='Delete Product' />";
+    echo "</form>";
+    
+    echo "</p>";
 }
-
-  //SQL query
-  $query = "SELECT id, name, description FROM products";
-  $result = $sql->query($query);
-
-  $num_results = $result->num_rows;
-
-  //For loop to display the items in our DB table
-  for ($i=0; $i <$num_results; $i++) {
-     $row = $result->fetch_assoc();
-     echo "</strong><br />Product ID: ";
-     echo stripslashes($row['id']);
-     echo "<br />Product Name: ";
-     echo stripslashes($row['name']);
-     echo "<br />Product Description: ";
-     echo stripslashes($row['description']);
-     
-     //Allows admin to change product info, connected to update_product.php
-     echo "<form method='post' action='update_product.php'>";
-     echo "<input type='int' name='id' value='" . $row['id'] . "' />";
-     echo "<input type='varchar' name='name' value='" . $row['name'] . "' />";
-     echo "<input type='text' name='description' value='" . $row['description'] . "'  />";
-     echo "<input type='submit' value='Update Product' />";
-     echo "</form>";
-     
-     
-     //Delete item button for admin use, connected to delete_product.php
-     echo "<form method='post' action='delete_product.php'>";
-     echo "<input type='hidden' name='id' value='" . $row['id'] . "' />";
-     echo "<input type='submit' value='Delete Product' />";
-     echo "</form>";
-     
-     echo "</p>";
-  }
-
-  $result->free();
-  $sql->close();
-
 ?>
 
 <!-- Add item, which goes to add_product.php -->
