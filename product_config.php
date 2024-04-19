eb48b33108347c84a93a31905a406fe354fc3ddc
Zainab099 <117556108+Zainab099@users.noreply.github.com>
2024-04-18 22:39:45 -0400

    Update product_config.php

--- product_config.php
+++ product_config.php
@@ -3,6 +3,5 @@ $host = "localhost";
 $username = "root";
 $password = "";
 $database = "product_rating_analysis";
-$conn = mysqli_connect($host, $username, $password, $database) or die('Could not connect');
-
+$sql = mysqli_connect($host, $username, $password, $database) or die('Could not connect');
 ?>
